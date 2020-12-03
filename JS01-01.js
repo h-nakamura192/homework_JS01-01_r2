a4a7d4138bc17fb7d4cfa2cae0b8a5bf407967ea
nakamura <gen.2polym.hn@gmail.com>
2020-12-03 06:26:57 +0000

    実装前のコード

---
+++ JS01-01.js
@@ -0,0 +1,12 @@
+var number =1;
+
+for(number=1;number<=99;number++){
+  if(number%15 == 0){
+    console.log("FizzBuzz " + number);
+  }else if(number%3 == 0){
+    console.log("Fizz " + number);
+  }else if(number%5 == 0){
+    console.log("Buzz " + number);
+  }
+}
+
